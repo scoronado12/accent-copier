af1eebf5dec3b5049982e072ef7a2ea89c3f2928
Stefano Coronado <stefanocorona12@gmail.com>
2018-12-23 10:59:54 -0500

    stripping out if-blocks in favor of dict handling

--- accent-copier.py
+++ accent-copier.py
@@ -3,17 +3,31 @@ import pyperclip as cp
 import sys
 
 
-spanish_chars = ["ñ", "á" , "é", "í", "ó", "ú" , "¿", "¡"]
-
-
+#spanish_chars = ["ñ", "á" , "é", "í", "ó", "ú" , "¿", "¡"]
+spanish_chars = dict(
+                    zip(['a','e','i','o', 'n','u','?','!'],
+                    ['á', 'é', 'í', 'ó', 'ñ', 'ú', '¿', '¡']))
+        
 def main():
     running = True
     
     while (running == True):
         print("Select a character to copy")
-        print(" 1) ñ \t 2) á \n 3) é \t 4) í \n 5) ó \t 6) ú \n 7) ¿ \t 8) ¡")
-            
+        print(" n) ñ \t a) á \n e) é \t i) í \n o) ó \t u) ú \n ?) ¿ \t !) ¡ \n q) quit")
+        
+        
+        
+        
+        
+        
         selection = input("Your Selection: ")
+
+        
+        print(selection)
+        charInCharOut(spanish_chars[selection])
+        
+        
+        '''
         if (selection == "1"):
             cp.copy(spanish_chars[0])
             print("Selected " + spanish_chars[0])
@@ -40,41 +54,21 @@ def main():
             cp.copy(spanish_chars[7])
         elif((selection == "quit") or (selection == "q")):
             print("¡Chau!")
-            exit()
+            exit(0)
         else:
             print("Nothing Selected")
-                        
+           '''             
 
 def charInCharOut(argv):
     
-        if (argv == "n"):
-            print(spanish_chars[0])
-            return (spanish_chars[0])
-            
-        elif (argv == "a"):
-            print(spanish_chars[1])
-            return (spanish_chars[1])
-            
-        elif (argv == "i"):
-            print(spanish_chars[2])
-            return (spanish_chars[2])
-            
-        elif (argv == "o"):
-            print(spanish_chars[3])
-            return (spanish_chars[3])
-            
-        elif(argv == "u"):
-            print(spanish_chars[4])
-            return (spanish_chars[4])
-            
-        elif(argv == "?"):
-            print(spanish_chars[5])
-            return (spanish_chars[5])
-        elif(argv == "!"):
-            print(spanish_chars[6])
-            return (spanish_chars[6])
-            
+    if argv not in spanish_chars:
+        print('Pass a proper char.')
+        exit(1)
     
+    char = spanish_chars[argv]
+    print(char)
+    return char
+
     
 
 
